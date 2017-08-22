6617f40bbbe6125f43a1213274632145f5270a3b
ddialar <ddialar@gmail.com>
2017-08-22 13:29:19 +0100

    feat(test.mutation): Include the needed code in order to get the user's token.

--- src/modules/graphql/mutations/test.mutation.js
+++ src/modules/graphql/mutations/test.mutation.js
@@ -10,6 +10,10 @@ var logger = log4js.getLogger();
 var runMutation = {
     type: GraphQLString,
     resolve: (parentValues, args, context) => {
+        logger.trace(`[context request keys  ] - ${Object.keys(context)}`);
+        logger.trace(`[context request header] - ${(context.request.headers) ? Object.keys(context.request.headers) : null}`);
+        logger.trace(`[context request header authorizaton] - ${(context.request.headers && context.request.headers.authorization) ? context.request.headers.authorization : null}`);
+        
         return 'GraphQL mutation OK';
     }
 };
